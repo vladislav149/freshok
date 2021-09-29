c770ed12a6be4f33ca0414507fd2596ef6c0df84
vladislav149 <vladislav149@mail.ru>
2021-09-30 01:47:45 +0300

    added mixitup

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,6 @@ $(function(){
     prevArrow: '<button type="button" class="slick-prev slick-arrows"></button>',
     nextArrow: '<button type="button" class="slick-next slick-arrows"></button>'
   });
+
+  var mixer = mixitup('.product');
 });
