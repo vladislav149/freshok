d15650e0b6969f88dea0e5c0b881c8abc0a2ba75
vladislav149 <vladislav149@mail.ru>
2021-11-21 23:56:48 +0300

    split the scss into components

--- app/js/main.js
+++ app/js/main.js
@@ -12,61 +12,37 @@ $(function(){
   });
 
   $('.catalog__btn').click(function() { // медленное появление и исчезновение
-    var catalog__list = $(this).next();
+    var catalog__items = $(this).next();
 
-    if(catalog__list.is(':visible'))
-        catalog__list.slideUp('slow');
+    if(catalog__items.is(':visible'))
+        catalog__items.slideUp('slow');
     else
-        catalog__list.slideDown('slow');
+        catalog__items.slideDown('slow');
   });
 
   $(document).mouseup(function (e){ // событие клика по веб-документу
-		var div = $('.catalog__list');   // тут указываем класс элемента
-		if (!$('.catalog__list').is(e.target) // если клик был не по этому блоку
-		    && $('.catalog__list').has(e.target).length === 0 // и не по его дочерним элементам
+		var div = $('.catalog__items');   // тут указываем класс элемента
+		if (!$('.catalog__items').is(e.target) // если клик был не по этому блоку
+		    && $('.catalog__items').has(e.target).length === 0 // и не по его дочерним элементам
         && !$('.catalog__btn').is(e.target)) { // и если не по кнопке
-			$('.catalog__list').slideUp('slow'); // медленно скрываю его
+			$('.catalog__items').slideUp('slow'); // медленно скрываю его
       $('.catalog__btn').removeClass('catalog__btn--active'); //удаляю класс
 		}
 	});
 
-  $('.banner__link').on('click',function(){
-    $('.basket').addClass('basket--active');
-    $('.overlay').addClass('overlay--active');
+  $('.user__btn').on('click',function(){
+    $('.basket').toggleClass('basket--active');
   });
 
-  $('.user__btn--cart').on('click',function(){
-    $('.basket').addClass('basket--active');
-    $('.overlay').addClass('overlay--active');
-  });
-  
-  $('.overlay').on('click',function(){
-    $('.basket').removeClass('basket--active');
-    $('.overlay').removeClass('overlay--active');
-  });
+  /* if (!$('.basket__active-part').is(e.target) // если клик был не по этому блоку
+		    && $('.basket__active-part').has(e.target).length === 0 // и не по его дочерним элементам
+        && !$('.user__btn').is(e.target)) { // и если не по кнопке
+      $('.basket').removeClass('basket--active'); //удаляю класс
+		} */
 
   $('.basket__close').on('click',function(){
     $('.basket').removeClass('basket--active');
-    $('.overlay').removeClass('overlay--active');
   });
-
-
-  $(document).ready(function() {
-            $('.btn--minus').click(function () {
-                var $input = $(this).parent().find('input');
-                var count = parseInt($input.val()) - 1;
-                count = count < 1 ? 1 : count;
-                $input.val(count);
-                $input.change();
-                return false;
-            });
-            $('.btn--plus').click(function () {
-                var $input = $(this).parent().find('input');
-                $input.val(parseInt($input.val()) + 1);
-                $input.change();
-                return false;
-            });
-        });
 
   var containerEl1 = document.querySelector('[data-ref="container-1"]');
   var containerEl2 = document.querySelector('[data-ref="container-2"]');
